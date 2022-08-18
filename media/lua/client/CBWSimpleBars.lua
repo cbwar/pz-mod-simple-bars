3177955a9799ca07c313960670824d4e8d4e7a78
Raphael Lisch <cbwar@protonmail.com>
2022-08-18 23:55:38 +0200

    initial commit

---
+++ media/lua/client/CBWSimpleBars.lua
@@ -0,0 +1,89 @@
+require("CBWSimpleBarsCommon")
+
+CBWSimpleBars = {}
+CBWSimpleBars.MOD_ID = "CBWSimpleBars"
+CBWSimpleBars.CONFIG_FILE = "config.json"
+CBWSimpleBars.VERSION = "0.1"
+CBWSimpleBars.DEBUG = true
+
+local function CBW_debug(message)
+    if CBWSimpleBars.DEBUG == false then
+        return
+    end
+    print(CBWSimpleBars.MOD_ID .. ": " .. message)
+end
+
+local function CBW_info(message)
+    print(CBWSimpleBars.MOD_ID .. ": " .. message)
+end
+
+CBW_info("---- Loading Mod -----")
+CBW_debug("MOD_ID = " .. CBWSimpleBars.MOD_ID)
+CBW_debug("CONFIG_FILE = " .. CBWSimpleBars.CONFIG_FILE)
+CBW_debug("VERSION = " .. CBWSimpleBars.VERSION)
+
+local config = CBW_decode_json(CBWSimpleBars.MOD_ID, CBWSimpleBars.CONFIG_FILE)
+if CBWSimpleBars.DEBUG then
+    CBW_dump_table(config)
+end
+CBW_info("---- Mod loaded successfully -----")
+
+local function CBWSimpleBars_on_create_player(playerIndex, isoPlayer)
+    CBW_debug("into create player #" .. playerIndex)
+
+    -- Make sure this is a local player only.
+    if not isoPlayer:isLocalPlayer() then
+        CBW_debug("player is not local")
+        return
+    end
+    frameRate = getPerformance():getFramerate()
+    tickRate = 60 + frameRate
+
+end
+
+Events.OnCreatePlayer.Add(CBWSimpleBars_on_create_player)
+
+-- CBWSimpleBars:loadConfig()
+-- print(CBW_SimpleBars.config["bars"])
+
+-- Bar = {
+--     ["height"] = 80,
+--     ["width"] = 10,
+-- }
+-- Bar.__index = Bar
+-- function Bar:debug()
+--     print("----- Bar")
+--     print("-- name = " .. self.name)
+--     print("-- position: " .. self.height .. "," .. self.width)
+--     print("-- color: " .. table.concat(self.color, ','))
+--     print("----- /Bar")
+-- end
+
+-- HealthBar = {}
+-- HealthBar.__index = HealthBar
+-- setmetatable(HealthBar, Bar)
+
+-- function HealthBar.new()
+--     local instance = setmetatable({}, HealthBar)
+--     instance.name = 'healthbar'
+--     instance.position = { 70, 70 }
+--     instance.color = { 255, 110, 0, 1 }
+--     return instance
+-- end
+
+-- function getBars()
+--     local bars = {}
+--         -- HealthBar.new()
+--     return bars
+-- end
+
+-- function debugBars()
+--     local bars = getBars()
+--     for i = 1, #bars do
+--         bars[i].debug()
+--     end
+-- end
+
+-- print("Mod Loaded")
+-- debugBars()
+-- Events.OnPlayerUpdate.Add(debugBars)
---
+++ media/lua/client/CBWSimpleBarsCommon.lua
@@ -0,0 +1,47 @@
+json = require("lib/json")
+
+local function CBW_debug(message)
+    if CBWSimpleBars.DEBUG == false then
+        return
+    end
+    print(CBWSimpleBars.MOD_ID .. ": " .. message)
+end
+
+--[[ 
+    Reads a json file and returns the contents in a table
+]]
+function CBW_decode_json(modId, filename)
+    CBW_debug("loading json file " .. filename)
+    local reader = getModFileReader(modId, filename, true)
+    if reader == nil then
+        reader = getFileReader(filename, true)
+    end
+
+    local contents = ""
+    local line = reader:readLine()
+    while line do
+        contents = contents .. line
+        line = reader:readLine()
+        if not line then
+            break
+        end
+    end
+    reader:close();
+    return json.decode(contents)
+end
+
+--[[
+    Dump table 
+]]
+function CBW_dump_table(tbl, indent)
+    indent = indent or 0
+    for k, v in pairs(tbl) do
+        formatting = string.rep("  ", indent) .. k .. ": "
+        if type(v) == "table" then
+            print(formatting)
+            CBW_dump_table(v, indent + 1)
+        else
+            print(formatting .. v)
+        end
+    end
+end
